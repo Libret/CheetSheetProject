b45bd089e275e62c5eeab2f3764f47814f007e31
Libret <70765177+Libret@users.noreply.github.com>
2022-10-28 16:51:44 +0200

    fixed the ")" absence

--- Model-View-Controller/Repositories/Migrations.cs
+++ Model-View-Controller/Repositories/Migrations.cs
@@ -8,7 +8,7 @@ namespace Model_View_Controller.Repositiries
         {
             SQLTableManagement.CreateTable(conn, "CREATE TABLE Topic (Id VARCHAR(20), Name VARCHAR(200))");
             SQLTableManagement.CreateTable(conn, "CREATE TABLE CheetSheetItem (Id VARCHAR(20), Name VARCAR(200), CodeSnippet TEXT, AdditionalInfo TEXT, TopicId VARCHAR(20))");
-            SQLTableManagement.CreateTable(conn, "CREATE TABLE UsefulLink (Id VARCHAR(20), LinkAddress VARCHAR(200), LinkOrder INT, CheetSheetItemId VARCHAR(20)");
+            SQLTableManagement.CreateTable(conn, "CREATE TABLE UsefulLink (Id VARCHAR(20), LinkAddress VARCHAR(200), LinkOrder INT, CheetSheetItemId VARCHAR(20))");
             
         }
     }
