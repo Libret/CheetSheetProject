100f20e14cdba3ac321257629b871868e9f528c8
Libret <70765177+Libret@users.noreply.github.com>
2022-10-28 17:37:11 +0200

    add class TopicRepository

---
+++ Model-View-Controller/Repositories/TopicRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Model_View_Controller.Repositories
+{
+    public class TopicRepository
+    {
+        public static void AddNewTopic(string topicName)
+        {
+            var Id = Guid.NewGuid();
+            SQLTableManagement.InsertData("Topic", "Id, Name", $"\"{Id}\", \"{topicName}\"");
+        }
+    }
+}
