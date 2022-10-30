564ec153f1c400e51d3bb62e2995e013290b94da
Libret <70765177+Libret@users.noreply.github.com>
2022-10-30 17:00:56 +0100

    added UsefulLinkRepository and CheetSheetItemRepository

---
+++ Model-View-Controller/Repositories/CheetSheetItemRepository.cs
@@ -0,0 +1,35 @@
+﻿using Model_View_Controller.Models;
+
+namespace Model_View_Controller.Repositories
+{
+    public class CheetSheetItemRepository
+    {
+        public static void AddNewCheetSheetItem(string cheetSheetItemName, string codeSnippet, string additionalInfo, string topicId)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData("CheetSheetItem", "Id, Name, CodeSnippet, AdditionalInfo, TopicId", $"\"{id}\", \"{cheetSheetItemName}\", \"{codeSnippet}\", \"{additionalInfo}\", \"{topicId}\"");
+        }
+
+        public static List<CheetSheetItem> GetAllCheetSheetItems()
+        {
+            var allCheetSheetItems = new List<CheetSheetItem>();
+            var sqlite_datareader = SQLTableManagement.ReadData("CheetSheetItem");
+            while (sqlite_datareader.Read())
+            {
+                string id = sqlite_datareader.GetString(0);
+                string name = sqlite_datareader.GetString(1);
+                string codeSnippet = sqlite_datareader.GetString(2);
+                string additionalInfo = sqlite_datareader.GetString(3);
+                allCheetSheetItems.Add(new CheetSheetItem
+                {
+                    Id = id,
+                    Name = name,
+                    CodeSnippet = codeSnippet,
+                    AdditionalInfo = additionalInfo,
+                });
+            }
+            SQLTableManagement.GetSQLiteConnection().Close();
+            return allCheetSheetItems;
+        }
+    }
+}
---
+++ Model-View-Controller/Repositories/UsefulLinkRepository.cs
@@ -0,0 +1,33 @@
+﻿using Model_View_Controller.Models;
+
+namespace Model_View_Controller.Repositories
+{
+    public class UsefulLinkRepository
+    {
+        public static void AddNewUsefulLink(string usefulLinkAdress, int order, string uheetSheetItemId)
+        {
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData("Topic", "Id, LinkAddress, LinkOrder, CheetSheetItemId", $"\"{id}\", \"{usefulLinkAdress}\", \"{order}\", \"{uheetSheetItemId}\"");
+        }
+
+        public static List<UsefulLink> GetAllTopics()
+        {
+            var allUsefulLinks = new List<UsefulLink>();
+            var sqlite_datareader = SQLTableManagement.ReadData("UsefulLink");
+            while (sqlite_datareader.Read())
+            {
+                string id = sqlite_datareader.GetString(0);
+                string usefulLinkAdress = sqlite_datareader.GetString(1);
+                string order = sqlite_datareader.GetString(2);
+                allUsefulLinks.Add(new UsefulLink
+                {
+                    Id = id,
+                    LinkAddress = usefulLinkAdress,
+                    Order = int.Parse(order)
+                });
+            }
+            SQLTableManagement.GetSQLiteConnection().Close();
+            return allUsefulLinks;
+        }
+    }
+}
