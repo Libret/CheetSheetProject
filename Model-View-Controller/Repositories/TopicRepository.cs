b332d839a89dfde7d2437584f2f3be6756342b58
Libret <70765177+Libret@users.noreply.github.com>
2022-10-30 17:05:18 +0100

    fixed writing identifier

--- Model-View-Controller/Repositories/TopicRepository.cs
+++ Model-View-Controller/Repositories/TopicRepository.cs
@@ -1,11 +1,34 @@
-﻿namespace Model_View_Controller.Repositories
+﻿using Model_View_Controller.Models;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Model_View_Controller.Repositories
 {
     public class TopicRepository
     {
         public static void AddNewTopic(string topicName)
         {
-            var Id = Guid.NewGuid();
-            SQLTableManagement.InsertData("Topic", "Id, Name", $"\"{Id}\", \"{topicName}\"");
+            var id = Guid.NewGuid();
+            SQLTableManagement.InsertData("Topic", "Id, Name", $"\"{id}\", \"{topicName}\"");
         }
+
+        public static List<Topic> GetAllTopics()
+        {
+            var allTopics = new List<Topic>();
+            var sqlite_datareader = SQLTableManagement.ReadData("Topic");
+            while (sqlite_datareader.Read())
+            {
+                string id = sqlite_datareader.GetString(0);
+                string name = sqlite_datareader.GetString(1);
+                allTopics.Add(new Topic
+                {
+                    Id = id,
+                    Name = name
+                });
+            }
+            SQLTableManagement.GetSQLiteConnection().Close();
+            return allTopics;
+        }
+
     }
 }
