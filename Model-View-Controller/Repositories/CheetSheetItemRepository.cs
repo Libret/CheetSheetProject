7d368fd132c2ea4fc79520dd40018bb2ac6e5287
Libret <70765177+Libret@users.noreply.github.com>
2022-11-02 15:47:21 +0100

    updated CheetSheetItemRepository

--- Model-View-Controller/Repositories/CheetSheetItemRepository.cs
+++ Model-View-Controller/Repositories/CheetSheetItemRepository.cs
@@ -4,22 +4,25 @@ namespace Model_View_Controller.Repositories
 {
     public class CheetSheetItemRepository
     {
+        private static readonly string stringCheetSheetItem = "CheetSheetItem";
+
         public static void AddNewCheetSheetItem(string cheetSheetItemName, string codeSnippet, string additionalInfo, string topicId)
         {
             var id = Guid.NewGuid();
-            SQLTableManagement.InsertData("CheetSheetItem", "Id, Name, CodeSnippet, AdditionalInfo, TopicId", $"\"{id}\", \"{cheetSheetItemName}\", \"{codeSnippet}\", \"{additionalInfo}\", \"{topicId}\"");
+            SQLTableManagement.InsertData(stringCheetSheetItem, "Id, Name, CodeSnippet, AdditionalInfo, TopicId", $"\"{id}\", \"{cheetSheetItemName}\", \"{codeSnippet}\", \"{additionalInfo}\", \"{topicId}\"");
         }
 
-        public static List<CheetSheetItem> GetAllCheetSheetItems()
+        public static List<CheetSheetItem> GetAllItems()
         {
             var allCheetSheetItems = new List<CheetSheetItem>();
-            var sqlite_datareader = SQLTableManagement.ReadData("CheetSheetItem", null);
+            var sqlite_datareader = SQLTableManagement.ReadData(stringCheetSheetItem, null);
             while (sqlite_datareader.Read())
             {
                 string id = sqlite_datareader.GetString(0);
                 string name = sqlite_datareader.GetString(1);
                 string codeSnippet = sqlite_datareader.GetString(2);
                 string additionalInfo = sqlite_datareader.GetString(3);
+                string topicId = sqlite_datareader.GetString(4);
                 allCheetSheetItems.Add(new CheetSheetItem
                 {
                     Id = id,
@@ -28,8 +31,76 @@ namespace Model_View_Controller.Repositories
                     AdditionalInfo = additionalInfo,
                 });
             }
-            SQLTableManagement.GetSQLiteConnection().Close();
+            SqliteConnect.CoseConnections(sqlite_datareader);
             return allCheetSheetItems;
+        }
+
+        public static List<CheetSheetItem> GetAllItemsByTopicId(string topicId)
+        {
+            var allCheetSheetItemsForTopic = new List<CheetSheetItem>();
+            var clause = $"TopicId = \"{topicId}\"";
+            var sqlite_datareader = SQLTableManagement.ReadData(stringCheetSheetItem, clause);
+            while (sqlite_datareader.Read())
+            {
+                string id = sqlite_datareader.GetString(0);
+                string name = sqlite_datareader.GetString(1);
+                string codeSnippet = sqlite_datareader.GetString(2);
+                string additionalInfo = sqlite_datareader.GetString(3);
+                
+                allCheetSheetItemsForTopic.Add(new CheetSheetItem
+                {
+                    Id = id,
+                    Name = name,
+                    CodeSnippet = codeSnippet,
+                    AdditionalInfo = additionalInfo,
+                });
+            }
+            SqliteConnect.CoseConnections(sqlite_datareader);
+            return allCheetSheetItemsForTopic;
+        }
+
+        public static CheetSheetItem? GetCheetSheetItem(string id)
+        {
+            string clause = $"id = \"{id}\"";
+            var sqlite_datareader = SQLTableManagement.ReadData(stringCheetSheetItem, clause);
+            while (sqlite_datareader.Read())
+            {
+                string name = sqlite_datareader.GetString(1);
+                string codeSnippet = sqlite_datareader.GetString(2);
+                string additionalInfo = sqlite_datareader.GetString(3);
+                SqliteConnect.CoseConnections(sqlite_datareader);
+                return new CheetSheetItem
+                {
+                    Id = id,
+                    Name = name,
+                    CodeSnippet = codeSnippet,
+                    AdditionalInfo = additionalInfo,
+                };
+            }
+            SqliteConnect.CoseConnections(sqlite_datareader);
+            return null;
+        }
+
+        public static void UpdateItemById(string id, CheetSheetItem cheetSheetItem)
+        {
+            var clause = $"Id = \"{id}\"";
+            var setItem = "";
+            setItem += $"Name = \"{cheetSheetItem.Name}\", ";
+            setItem += $"CodeSnippet = \"{cheetSheetItem.CodeSnippet}\", ";
+            setItem += $"AdditionalInfo = \"{cheetSheetItem.AdditionalInfo}\"";
+            SQLTableManagement.UpdateData(CheetSheetItemRepository.stringCheetSheetItem, setItem, clause);
+        }
+
+        public static void DeleteItemById(string id)
+        {
+            var clause = $"Id = \"{id}\"";
+            SQLTableManagement.DeleteData(stringCheetSheetItem, clause);
+        }
+
+        public static void DeleteItemByName(string name)
+        {
+            var clause = $"Name = \"{name}\"";
+            SQLTableManagement.DeleteData(stringCheetSheetItem, clause);
         }
     }
 }
