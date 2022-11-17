92e00e748766620f57f1a2209016acaadf5d8347
Libret <70765177+Libret@users.noreply.github.com>
2022-11-17 13:03:46 +0100

    Update Program.cs

--- Model-View-Controller/Program.cs
+++ Model-View-Controller/Program.cs
@@ -1,3 +1,5 @@
+using Model_View_Controller.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +26,37 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-Model_View_Controller.Repositories.Migrations.Run();
-Model_View_Controller.Repositories.TopicRepository.AddNewTopic("git");
+//Model_View_Controller.Repositories.Migrations.Run();
+/*
+
+
+
+var allItemsForTopic = Model_View_Controller.Repositories.TopicRepository.GetTopicWithAllItems("a8c4f9ac-9092-4e20-864c-1001bc31d5d1");
+
+
+Model_View_Controller.Repositories.UsefulLinkRepository.AddNewUsefulLink(firstUsefulLink, "871420ef-744d-4900-9774-34f3fef587f2");
+
+var newItem = new CheetSheetItem
+{
+    Name = "useful command",
+    AdditionalInfo = "basic tips for git navigation and interaction",
+    CodeSnippet = ""
+};
+
+Model_View_Controller.Repositories.CheetSheetItemRepository.AddNewCheetSheetItem(newItem, "a8c4f9ac-9092-4e20-864c-1001bc31d5d1");
+*/
+var firstUsefulLink = new UsefulLink
+{
+    LinkAddress = "https://training.github.com/downloads/github-git-cheat-sheet/",
+    LinkOrder = 0
+};
+Model_View_Controller.Repositories.UsefulLinkRepository.AddNewUsefulLink(firstUsefulLink, "7e052580-0ad1-4b66-b0ce-9fc62136c642");
+
+var secondUsefulLink = new UsefulLink
+{
+    LinkAddress = "https://www.baeldung.com/git-guide",
+    LinkOrder = 0
+};
+Model_View_Controller.Repositories.UsefulLinkRepository.AddNewUsefulLink(secondUsefulLink, "7e052580-0ad1-4b66-b0ce-9fc62136c642");
+
 app.Run();
